6cb6df267e89f7aeb19ee583aa21d29bfae64a49
Steven Liang <rockboyfor@gmail.com>
2015-09-07 17:55:16 +0800

    Moon Checker 0.1

--- MoonScanner/MoonScanner.py
+++ MoonScanner/MoonScanner.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 import checkRule
 import StatusCode
+import siteReader
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -16,18 +17,51 @@ class checker(object):
         self.goodSet = set()
         # url that has been proved bad :(
         self.badSet = set()
+        # Parent URL count
+        self.count = 0
     def check(self, url):
         # all the urls in one url's content
         siteDict = {}
         # check result
         passCheck = True
         # good/bad result
-        goodResult = '\n'
-        badResult = '\n'
+        goodResult = ''
+        badResult = ''
 
-        # open url to get page content
-        html = urllib2.urlopen(url).read()
+        # check for the url given
+        firstResult = self.getCheckResult(url, 'Parent Link Error')
+        # parent url error
+        if not firstResult[0]:
+            badResult = badResult + firstResult[1]
+        else:
+            # open parent url to get page content
+            html = urllib2.urlopen(url).read()
 
+            # extract our content between '<section class="wa-section">...</section>'
+            siteDict = self.parser(html)
+
+            # Now we got site dictionary in ('url','text'), let's check
+            for k, v in siteDict.iteritems():
+                result = self.getCheckResult(k,v)
+                if result[0]:
+                    goodResult = goodResult + result[1]
+                else:
+                    badResult = badResult + result[1]
+
+            # extract all the link into our innerList
+            #with open('OK.txt','w') as good:
+            #    good.write(goodResult)
+            
+            #with open('BAD.txt', 'w') as bad:
+            #    bad.write(badResult)
+
+            # Final message
+        return badResult, goodResult
+
+
+    def parser(self,html):
+        # define temp dict to hold site dictionary
+        tempDict = {}
         # extract our content between '<section class="wa-section">...</section>'
         startPos = html.index(self.startStr)
         endPos = html.index(self.endStr, startPos)
@@ -41,50 +75,59 @@ class checker(object):
                 # relative link
                 if href.strip().startswith('/'):
                     href = self.host + href
-                siteDict[href] = link.contents[0]
+                tempDict[href] = link.contents[0]
 
-        # Now we got site dictionary in ('url','text'), let's check
-        for k, v in siteDict.iteritems():
-            # Check if the url is already in our goodSites or badSites
-            if k in self.goodSet:
-                goodResult = v + '\t' + k + '\tOK\n'
-                continue
-            if k in self.badSet:
-                passCheck = False
-                badResult = v + '\t' + k + '\tBroken Link' + str(status) +'\n'
-                continue
-            # New url
-            myCheckRule = checkRule.checkRule(k)
-            status = myCheckRule.startCheck()
-            # if status good, add to goodSet
-            if status == StatusCode.StatusCode.OK:
-                self.goodSet.add(k)
-                goodResult = v + '\t' + k + '\tOK\n'
-            else:
-                passCheck = False
-                self.badSet.add(k)
-                badResult = v + '\t' + k + '\tBad Link'+ str(status) +'\n'
+        return tempDict
 
-        # extract all the link into our innerList
-        #with open('OK.txt','w') as good:
-        #    good.write(goodResult)
+    def getCheckResult(self, url, name):
+        # CheckResult flag
+        checkFlag = True
+        # Check result string
+        result = '\n'+ name + '\t' + url + '\t'
+        # Check if the url is already in our goodSites or badSites
+        if url in self.goodSet:
+            result = result + 'OK\n'
+            # return checkFlag, result
+        if url in self.badSet:
+            checkFlag = False
+            result = result + 'BAD\n'
             
-        #with open('BAD.txt', 'w') as bad:
-        #    bad.write(badResult)
-
-        # Final message
-        
-        if passCheck:
-            return 'Clean page!'
+        # New url
+        myCheckRule = checkRule.checkRule(url)
+        status = myCheckRule.startCheck()
+        # if status good, add to goodSet
+        if status == StatusCode.StatusCode.OK:
+            self.goodSet.add(url)
+            result = result + '  ' + str(StatusCode.StatusCode.OK) +'\n'
         else:
-            print 'Found broken links!'
-            return badResult
+            checkFlag = False
+            self.badSet.add(url)
+            result = result + '  ' + str(status) +'\n'
+
+        return checkFlag, result
+
+
 
 
 if __name__ == '__main__':
-    myChecker = checker()
-    result = myChecker.check('http://wacn-ppe.chinacloudsites.cn/zh-cn/documentation/articles/sql-database-index-advisor')
+    myReader = siteReader.siteReader('site.txt')
+    siteList = myReader.getSiteList()
+    count = 0
+    for url in siteList:
+        count += 1
+        myChecker = checker()
+        result = myChecker.check(url)
 
-    print result
-    #with open('test.html','w') as html:
-    #    html.write(result)
+        # print "------------------------------BAD LINK-----------------------------------"
+        # print result[0]
+        # print "------------------------------GOOD LINK----------------------------------"
+        # print result[1]
+        with open('bad.txt','a') as bad:
+            bad.write('------------------------Parent Link - ' + str(count) + '----------------------------')
+            bad.write(result[0].encode('utf-8'))
+        with open('good.txt','a') as good:
+            good.write('------------------------Parent Link - ' + str(count) + '----------------------------')
+            good.write(result[1].encode('utf-8'))
+
+
+    print ('All url has been checked, please check out "bad.txt" and "good.txt" for detailed information')
--- MoonScanner/StatusCode.py
+++ MoonScanner/StatusCode.py
@@ -6,6 +6,4 @@
     MoonCake_Not_Found = 404
     # 500
     MoonCake_Internal_Server_Error = 500
-    # Unkown Status Code
-    Unknown_Status_Code = 9999
 
